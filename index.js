20dd511bf349e85a45d0b0fa597647c8608789b0
Dmitry <df.creative@gmail.com>
2016-03-31 23:28:39 -0400

    Normalize newlines

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ function clean (str) {
 	//remove indentation/tabulation
 	.replace(/^\s*/gm, '')
 
+	//transform all \r to \n
+	.replace(/[\n\r]+/g, '\n')
+
 	//replace duble spaces/tabs to single ones
-	.replace(/(\s)\s+/g, '$1');
+	.replace(/(\s)\s+/g, '$1')
 }
 
 module.exports = clean;
